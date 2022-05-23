fae11813fb0e8482d02548f730141d7b52f44269
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-05-23 08:18:21 +0300

    feat: add variable for script

---
+++ client/script.js
@@ -0,0 +1,10 @@
+const viewAddStudentButton = document.querySelector('.button_viewModalAdd');
+const delStudentButton = document.querySelector('.button_del-student')
+const getStudentsButton = document.querySelector('.button_get-students');
+const studentCard = document.querySelector('.student-card');
+const closeButton = document.querySelector('.button_close');
+
+const main = document.querySelector('.main');
+const studentCardWrapper = document.querySelector('.student-card-wrapper');
+
+const studentList = document.querySelector('.modal_add-user');
