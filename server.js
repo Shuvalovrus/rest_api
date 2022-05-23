27652efc1605a8aa6ef5b257a1875aca753e7b6d
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-05-23 08:25:54 +0300

    feat: create db connectrion

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const mySql = require('mysql2')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3500
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+const connection = mySql.createConnection({
+    host: 'localhost',
+    port: '85',
+    user: 'root',
+    password: 'root',
+    database: 'users'
+})
+
+connection.connect()
